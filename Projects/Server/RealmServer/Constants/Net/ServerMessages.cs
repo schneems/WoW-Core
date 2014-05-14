6d20435dc5c07e044a484839d5cafbac584642ea
Fabian <fabian@arctium.org>
2014-05-14 18:54:55 +0200

    Fixed RealmServer project path in solution. Sort RealmServer ServerMessages enum with regions. Based on the class where the handler is called in the client.

--- Projects/Server/RealmServer/Constants/Net/ServerMessages.cs
+++ Projects/Server/RealmServer/Constants/Net/ServerMessages.cs
@@ -19,36 +19,59 @@ namespace RealmServer.Constants.Net
 {
     public enum ServerMessages : ushort
     {
-        SetVeteranTrial                         = 0x2000,
-        AuthResponse                            = 0x2000,
-        WaitQueueUpdate                         = 0x2000,
-        WaitQueueFinish                         = 0x2000,
-        EnumCharactersResult                    = 0x2000,
+        #region CharacterService
+        CharacterUpgradeStarted                 = 0x2000,
+        CharacterUpgradeComplete                = 0x2000,
+        CharacterUpgradeAborted                 = 0x2000,
+        #endregion
+
+        #region GlueMgr
         GenerateRandomCharacterNameResult       = 0x2000,
-        UpdateExpansionLevel                    = 0x2000,
+        FeatureSystemStatusGlueScreen           = 0x2000,
+        MoveCharacterCheatSuccess               = 0x2000,
+        MoveCharacterCheatFailure               = 0x2000,
+        DisplayPromotion                        = 0x2000,
+        SetPromotionResponse                    = 0x2000,
+        RealmSplit                              = 0x2000,
+        SetPlayerDeclinedNamesResult            = 0x2000,
         CharCustomize                           = 0x2000,
-        CreateChar                              = 0x2000,
-        DeleteChar                              = 0x2000,
-        CacheVersion                            = 0x2000,
-        CharacterLoginFailed                    = 0x2000,
-        AccountDataTimes                        = 0x2000,
-        AddonInfo                               = 0x2000,
-        GetAccountCharacterListResult           = 0x2000,
+        CheatPlayerLookup                       = 0x2000,
         LiveRegionGetAccountCharacterListResult = 0x2000,
         CharacterRenameResult                   = 0x2000,
         LiveRegionCharacterCopyResult           = 0x2000,
         LiveRegionAccountRestoreResult          = 0x2000,
-        CharacterObjectTestResponse             = 0x2000,
         CharFactionChangeResult                 = 0x2000,
-        CharacterUpgradeStarted                 = 0x2000,
-        CharacterUpgradeComplete                = 0x2000,
-        CharacterUpgradeAborted                 = 0x2000,
         UpdateCharacterFlags                    = 0x2000,
         UndeleteCharacterResponse               = 0x2000,
         UndeleteCooldownStatusResponse          = 0x2000,
+        #endregion
 
-        #region Connection
+        #region Misc
+        UpdateAccountData                       = 0x2000,
+        AccountDataTimes                        = 0x2000,
+        #endregion
+
+        #region RealmConnection
+        SetVeteranTrial                         = 0x2000,
         AuthChallenge                           = 0x2000,
+        AuthResponse                            = 0x2000,
+        WaitQueueUpdate                         = 0x2000,
+        WaitQueueFinish                         = 0x2000,
+        EnumCharactersResult                    = 0x2000,
+        UpdateExpansionLevel                    = 0x2000,
+        AddonInfo                               = 0x2000,
+        SetTimeZoneInformation                  = 0x2000,
+        CreateChar                              = 0x2000,
+        DeleteChar                              = 0x2000,
+        CacheVersion                            = 0x2000,
+        CharacterLoginFailed                    = 0x2000,
+        LogoutResponse                          = 0x2000,
+        LogoutComplete                          = 0x2000,
+        LogoutCancelAck                         = 0x2000,
+        GetAccountCharacterListResult           = 0x2000,
+        #endregion
+
+        #region NetClient
         Pong                                    = 0x2000,
         #endregion
     }
