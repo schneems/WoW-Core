8386eb54533660c660144c12910cf06c2756e81f
Fabian <fabian@arctium.org>
2014-05-26 06:03:24 +0200

    Implemented CreateGameAccount command.

--- Projects/Server/AuthServer/Commands/AccountCommands.cs
+++ Projects/Server/AuthServer/Commands/AccountCommands.cs
@@ -30,7 +30,7 @@ namespace AuthServer.Commands
     class AccountCommands
     {
         [ConsoleCommand("CreateAccount", "")]
-        public static void Create(string[] args)
+        public static void CreateAccount(string[] args)
         {
             var email = Command.Read<string>(args, 0);
             var password = Command.Read<string>(args, 1);
@@ -55,41 +55,77 @@ namespace AuthServer.Commands
                     };
 
                     if (DB.Auth.Add(account))
-                    {
-                        // Default class/expansion data (sent in AuthResponse)
-                        var defaultAllowedClasses = new byte[,] { { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 2 }, 
-                                                                  { 7, 0 }, { 8, 0 }, { 9, 0 }, { 10, 4 }, { 11, 0 } };
-
-                        // Default race/expansion data (sent in AuthResponse)
-                        var defaultAllowedRaces = new byte[,] { { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 0 }, 
-                                                                { 7, 0 }, { 8, 0 }, { 9, 3 }, { 10, 1 }, { 11, 1 }, { 22, 3 }, 
-                                                                { 24, 4 }, { 25, 4 }, { 26, 4 }};
-
-                        for (int i = 0; i < defaultAllowedClasses.Length / 2; i++)
-                        {
-                            DB.Auth.Add(new AllowedClass
-                            {
-                                AccountId = account.Id,
-                                Class     = defaultAllowedClasses[i, 0],
-                                Expansion = defaultAllowedClasses[i, 1]
-                            });
-                        }
-
-                        for (int i = 0; i < defaultAllowedRaces.Length / 2; i++)
-                        {
-                            DB.Auth.Add(new AllowedRace
-                            {
-                                AccountId = account.Id,
-                                Race      = defaultAllowedRaces[i, 0],
-                                Expansion = defaultAllowedRaces[i, 1]
-                            });
-                        }
-
                         Log.Message(LogType.Normal, "Account {0} successfully created", email);
-                    }
                 }
                 else
                     Log.Message(LogType.Error, "Account {0} already in database", email);
+            }
+        }
+
+        [ConsoleCommand("CreateGameAccount", "")]
+        public static void CreateGameAccount(string[] args)
+        {
+            var accountId = Command.Read<int>(args, 0);
+            var game = Command.Read<string>(args, 1);
+            var index = Command.Read<byte>(args, 2);
+
+            if (accountId != 0 && game != "" && index != 0)
+            {
+                var account = DB.Auth.Accounts.SingleOrDefault(a => a.Id.Equals(accountId));
+
+                if (account != null)
+                {
+                    var exists = account.GameAccounts != null ? account.GameAccounts.Any(ga => ga.Game == game && ga.Index == index) : false;
+
+                    if (!exists)
+                    {
+                        var gameAccount = new GameAccount
+                        {
+                            AccountId = account.Id,
+                            Game      = game,
+                            Index     = index,
+                            Region    = Regions.XX,
+                            Flags     = GameAccountFlags.None,
+                            BoxLevel  = 5
+                        };
+
+                        if (DB.Auth.Add(gameAccount))
+                        {
+                            // Default class/expansion data (sent in AuthResponse)
+                            var defaultAllowedClasses = new byte[,] { { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 2 },
+                                                                    { 7, 0 }, { 8, 0 }, { 9, 0 }, { 10, 4 }, { 11, 0 } };
+
+                            // Default race/expansion data (sent in AuthResponse)
+                            var defaultAllowedRaces = new byte[,] { { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 0 },
+                                                                  { 7, 0 }, { 8, 0 }, { 9, 3 }, { 10, 1 }, { 11, 1 }, { 22, 3 },
+                                                                  { 24, 4 }, { 25, 4 }, { 26, 4 }};
+
+                            for (int i = 0; i < defaultAllowedClasses.Length / 2; i++)
+                            {
+                                DB.Auth.Add(new AllowedClass
+                                {
+                                    AccountId = gameAccount.Id,
+                                    Class     = defaultAllowedClasses[i, 0],
+                                    Expansion = defaultAllowedClasses[i, 1]
+                                });
+                            }
+
+                            for (int i = 0; i < defaultAllowedRaces.Length / 2; i++)
+                            {
+                                DB.Auth.Add(new AllowedRace
+                                {
+                                    AccountId = gameAccount.Id,
+                                    Race      = defaultAllowedRaces[i, 0],
+                                    Expansion = defaultAllowedRaces[i, 1]
+                                });
+                            }
+
+                            Log.Message(LogType.Normal, "GameAccount '{0}{1}' for Account '{2}' successfully created.", game, index, accountId);
+                        }
+                    }
+                }
+                else
+                    Log.Message(LogType.Error, "Account '{0}' doesn't exist.", accountId);
             }
         }
     }
