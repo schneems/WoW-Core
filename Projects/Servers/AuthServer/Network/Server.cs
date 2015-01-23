0af01845e98d07a59b2cde85cb8879458fc9dd8f
Fabian <fabian@arctium.org>
2015-01-23 06:16:37 +0100

    My fail...

--- Projects/Servers/AuthServer/Network/Server.cs
+++ Projects/Servers/AuthServer/Network/Server.cs
@@ -25,6 +25,8 @@ namespace AuthServer.Network
 {
     class Server : ServerBase
     {
+        public Server(string ip, int port) : base(ip, port) { }
+
         public override async Task DoWork(Socket client)
         {
             var clientId = ++Manager.SessionMgr.LastSessionId;
