3fdcec536bba475479e31cdd22d68925b8f6738c
Fabian <fabian@arctium.org>
2015-04-04 17:46:44 +0200

    Re-license code under MIT license.

--- Projects/Servers/CharacterServer/Packets/Server/Misc/TutorialFlags.cs
+++ Projects/Servers/CharacterServer/Packets/Server/Misc/TutorialFlags.cs
@@ -1,19 +1,5 @@
-﻿/*
- * Copyright (C) 2012-2015 Arctium Emulation <http://arctium.org>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+﻿// Copyright (c) Arctium Emulation.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using CharacterServer.Constants.Net;
 using Framework.Network.Packets;
