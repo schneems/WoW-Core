c886606901b3ab46b1f572a0e107d5024053ac22
Fabian <fabian@arctium-emulation.com>
2016-10-07 04:18:30 +0200

    Initialize new folder struct.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Project WoW is a World of Warcraft Warlord of Draenor emulator (in development).
-
+Project WoW is a World of Warcraft Legion emulator (in development).
+###
 [![Join the chat at https://gitter.im/Arctium-Emulation/Project-WoW](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/Arctium-Emulation/Project-WoW?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+###
+[![Build status](https://ci.appveyor.com/api/projects/status/aaoegmcmp5emmom4/branch/master?svg=true)](https://ci.appveyor.com/project/Fabi/project-wow/branch/master)
---
+++ Source/Libraries/Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Framework")]
+[assembly: AssemblyCopyright("Copyright ©  116")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("32FA0C9D-D01B-456E-A24A-AD979647F4C3")]
+
