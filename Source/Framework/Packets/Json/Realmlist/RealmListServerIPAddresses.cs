0492b64e74884196cfdc1c39c713505f66102980
Fabian <fabian@arctium.io>
2016-12-06 23:03:41 +0100

    Added Json classes used for authentication & realmlist.

---
+++ Source/Framework/Packets/Json/Realmlist/RealmListServerIPAddresses.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Arctium Emulation.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Framework.Packets.Json.Misc;
+
+namespace Framework.Packets.Json.Realmlist
+{
+    [DataContract]
+    public class RealmListServerIPAddresses
+    {
+        [DataMember(Name = "families")]
+        public IList<RealmListServerIPAddress> Families { get; set; }
+    }
+}
---
+++ Source/Framework/Packets/Json/Realmlist/RealmListUpdates.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Arctium Emulation.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Framework.Packets.Json.Realmlist
+{
+    [DataContract]
+    public class RealmListUpdates
+    {
+        [DataMember(Name = "updates")]
+        public IList<RealmListUpdatePart> Updates { get; set; }
+    }
+}
