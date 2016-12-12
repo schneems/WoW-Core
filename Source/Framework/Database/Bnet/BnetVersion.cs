b6321559a921f7b448689132ab0e7958d6bd8205
Fabian <fabian@arctium.io>
2016-12-12 04:38:01 +0100

    Added Bnet database entities & base sql file (MySql).

---
+++ Source/Framework/Database/Bnet/BnetVersion.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Arctium Emulation.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using LappaORM;
+
+namespace Framework.Database.Bnet
+{
+    public class BnetVersion : Entity
+    {
+        public string Version { get; set; }
+    }
+}
