37b89052d39934011332b6bb9bec91f70f1933b5
Fabian <fabian@arctium.io>
2017-07-24 14:11:28 +0200

    Config -> ConfigBase. Added test config class to Arctium.Manager. Added default.props (shared project properties).

---
+++ src/Arctium.Manager/src/Misc/Config.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Arctium Emulation.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Arctium.Core.Configuration;
+using Arctium.Core.Logging;
+
+namespace Arctium.Manager.Misc
+{
+    class Config : ConfigBase<Config>
+    {
+        [ConfigEntry("Log.Level", LogTypes.All)]
+        public static LogTypes LogLevel { get; }
+
+        [ConfigEntry("Log.Directory", "Logs/Manager")]
+        public static string LogDirectory { get; }
+
+        [ConfigEntry("Log.Console.File", "Console.log")]
+        public static string LogConsoleFile { get; }
+    }
+}
